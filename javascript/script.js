11fcf8402f64623c4e0c36087a62891c5d715f3a
Tyomay1999 <artyom.bordulanyk@gmail.com>
2020-06-08 01:13:08 +0400

    everything has changed

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,6 @@
 const API_URL = 'https://api.openweathermap.org/data/2.5/';
-const API_KEY = 'fd48bdf8a8b87b3c140f17625f4e2d57';
-const API_IMG = 'http://openweathermap.org/img/w/'
+const API_KEY = '40a3ab422b6c7446253471c3714edfb8';
+const API_IMG = 'http://openweathermap.org/img/w/';
 // Geolocation Cuurent data Weather
 // `${API_URL}weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`; 
 // Forecast
@@ -8,59 +8,80 @@ const API_IMG = 'http://openweathermap.org/img/w/'
 //  Search City name
 // `${API_URL}weather?q=${inputValue}&appid=${API_KEY}&units=metric`
 // `${API_URL}forecast?q=${inputValue}&appid=${API_KEY}&units=metric`
-// const iconFunn = (i) =>{
-//     document.getElementById
-// }
-// const icon = document.getElementById(`icon${i}`)
+
+
+
 const state = {
-    mwd: null,
     icon: null,
     temp: null,
     humidity: null,
-    description: null
-}
-const setState = (data, i) => {
-    state.mwd = data.list[i].dt_txt;
-    state.icon = data.list[i].weather[0].icon;
-    state.temp = data.list[i].main.temp;
-    state.description = data.list[i].weather[0].description;
-    state.humidity = data.list[i].main.humidity
-}
+    description: null,
+    city: null,
+    country: null,
+    lat: null,
+    lon: null,
+    list: null
+};
+//---------------------Helping functions-----------------------------------------------
+const setQS = (selector, value) => {
+    document.querySelector(selector).innerText = value; 
+};
+
 byId = (id, value) => {
     document.getElementById(id).innerHTML = value
 }
 const renderData = (g) => {
-    byId(`mwd${g}`, `${state.mwd}`);
+
     byId(`temp${g}`, `${state.temp}`);
     byId(`humidity${g}`, `${state.humidity}%`)
     byId(`description${g}`, `${state.description}`)
     const icon = document.getElementById(`icon${g}`).src = `${API_IMG}${state.icon}.png`
 }
+const setState = (data, i) => {
+    state.icon = data.list[i].weather[0].icon;
+    state.temp = data.list[i].main.temp;
+    state.description = data.list[i].weather[0].description;
+    state.humidity = data.list[i].main.humidity
+
+}
+
+//------------------------------------------------------------------------------
+
 const gettingWeather = async () => {
     const inputValue = document.getElementById('input').value;
     if (inputValue) {
-        document.getElementById('messageError').innerHTML = ''
-        const response = await fetch(`${API_URL}forecast?q=${inputValue}&appid=${API_KEY}&units=metric`)
-        const data = await response.json();
-        setState(data, 7);
-        renderData(2);
-        setState(data, 15);
-        renderData(3);
-        setState(data, 23);
-        renderData(4);
-        setState(data, 31);
-        renderData(5);
-        setState(data, 39);
-        renderData(6);
-    } else {
-        document.getElementById('messageError').innerHTML = 'Enter City Name'
-    }
+        setQS('#messageError', '');
+        (async function () {
+            const response = await fetch( `${API_URL}forecast?q=${inputValue}&APPID=${API_KEY}&units=metric` );
+            if (response.status == 404) {
+                setQS('#messageError', 'Please, enter the correct city name!');
+            } else {
+                const data = await response.json();
+                setState(data, 7);
+                renderData(2);
+                setState(data, 15);
+                renderData(3);
+                setState(data, 23);
+                renderData(4);
+                setState(data, 31);
+                renderData(5);
+                setState(data, 39);
+                renderData(6);
+                setStateOne(data);
+                renderOneData();
+
+            }   
+        })();
+    } else setQS( '#messageError', 'Please, enter the city name!' );
 }
+
+//----------------input keyup ENTER-------------------------------------
+
 const enter = (event) => {
-    if (event.key === 'Enter') {
-        gettingWeather()
-    }
-}
+    if (event.key === 'Enter') gettingWeather()  
+};
+
+//---------------------------Mouse------------------------------------------------
 const mouse = (name) => {
     document.getElementById(name).addEventListener('mouseover', () => {
         document.getElementById('show').style.display = "grid";
@@ -69,89 +90,9 @@ const mouse = (name) => {
         document.getElementById('show').style.display = "none";
     });
 };
+
 mouse('two');
 mouse('three');
 mouse('four');
 mouse('five');
 mouse('six')
-
-
-
-//Qo kodi het chenq karace miacnenq problema tali mer mas@ mer gracov lav ahxatuma .. nerqevin@ qonna
-
-
-
-// const API_URL = 'https://api.openweathermap.org/data/2.5/';
-// const TEMP_URL = 'http://openweathermap.org/img/w/';
-// const API_KEY = '40a3ab422b6c7446253471c3714edfb8';
-// // Geolocation Cuurent data Weather
-// // `${API_URL}weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`; 
-// // Forecast
-// // `${API_URL}forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
-// //  Search City name
-// // `${API_URL}weather?q=${inputValue}&appid=${API_KEY}&units=metric`
-// // `${API_URL}forecast?q=${inputValue}&appid=${API_KEY}&units=metric`
-// const state = {
-//     // mwd: null,
-//     icon: null,
-//     temp: null,
-//     humidity: null,
-//     description: null,
-//     city: null,
-//     country: null,
-//     lat: null,
-//     lon: null,
-//     list: null
-// };
-// //---------------------Helping functions-----------------------------------------------
-// const setQS = (selector, value) => {
-//     document.querySelector(selector).innerText = value; 
-// };
-// byId = (id, value) => {
-//     document.getElementById(id).innerHTML = value
-// }
-// const renderData = (g) => {
-//     byId(`temp${g}`, `${state.temp}`);
-//     byId(`humidity${g}`, `${state.humidity}%`)
-//     byId(`description${g}`, `${state.description}`)
-//     const icon = document.getElementById(`icon${g}`).src = `${API_IMG}${state.icon}.png`
-// }
-// const setState = (data, i) => {
-//     state.icon = data.list[i].weather[0].icon;
-//     state.temp = data.list[i].main.temp;
-//     state.description = data.list[i].weather[0].description;
-//     state.humidity = data.list[i].main.humidity
-// }
-// //------------------------------------------------------------------------------
-// const gettingWeather = async () => {
-//     const inputValue = document.getElementById('input').value;
-//     if (inputValue) {
-//         setQS('#messageError', '');
-//         (async function () {
-//             const response = await fetch( `${API_URL}forecast?q=${inputValue}&APPID=${API_KEY}&units=metric` );
-//             if (response.status == 404) {
-//                 setQS('#messageError', 'Please, enter the correct city name!');
-//             } else {
-//                 const data = await response.json();
-//                 setStateOne(data);
-//                 renderOneData();
-//                 setState(data, 7);
-//                 renderData(2);
-//                 setState(data, 15);
-//                 renderData(3);
-//                 setState(data, 23);
-//                 renderData(4);
-//                 setState(data, 31);
-//                 renderData(5);
-//                 setState(data, 39);
-//                 renderData(6);
-//                 setStateOne(data);
-//                 renderOneData();
-//             }   
-//         })();
-//     } else setQS( '#messageError', 'Please, enter the city name!' );
-// }
-// //----------------input keyup ENTER-------------------------------------
-// const enter = (event) => {
-//     if (event.key === 'Enter') gettingWeather()  
-// };
--- javascript/script_1.js
+++ javascript/script_1.js
@@ -1,92 +1,103 @@
-// const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-// const months = ["January", "February", "March", "April", "May", "June", "July", "August",
-//                 "September", "October", "November", "December"];
-// const thisDayInfo = () => {
-//     const day = new Date();
-//     setQS('.weekDay', days[day.getDay()]);
-//     setQS('.monthDay', `${months[day.getMonth()]}, ${day.getDate()}`);
-//     console.log(day)
-// };
-// const nextDayInfo = (QS, i) => {
-//     const day = new Date(state.list[i].dt_txt.slice(0,10));
-//     setQS(QS,`${days[day.getDay()]} - ${months[day.getMonth()]} - ${day.getDate()}`);
-// };
-// const callNextDayInfo = () => {
-//     nextDayInfo('#mwd2',7);
-//     nextDayInfo('#mwd3',15);
-//     nextDayInfo('#mwd4',23);
-//     nextDayInfo('#mwd5',31);
-//     nextDayInfo('#mwd6',39)
-// };
-// const setStateOne = (data) => {
-//     state.city = data.city.name;
-//     state.temp = data.list[0].main.temp;
-//     state.humidity = data.list[0].main.humidity;
-//     state.country = data.city.country;
-//     state.description = data.list[0].weather[0].description;
-//     state.lat = data.city.coord.lat;
-//     state.lon = data.city.coord.lon;
-//     state.icon = data.list[0].weather[0].icon;
-//     state.list = data.list;
-// };
-// const renderOneData = () => {
-//     setQS('.city', `${state.city}, ${state.country}`);
-//     setQS('.temp', `${Math.round(state.temp)} °C`);
-//     setQS('.humidityGet', `${state.humidity}%`);
-//     setQS('.cloud', `${state.description.charAt(0).toUpperCase() + state.description.slice(1)}`);
-//     document.querySelector('.icon').src = `${TEMP_URL}${state.icon}.png`;
-//     setQS('.long_lang', `[${state.lon.toFixed(2)}, ${state.lat.toFixed(2)}]`);
-// };
-// //---------------Get longitude and latitude------------------------------------
-// (function getDate() {
-//     window.navigator.geolocation.getCurrentPosition((position) => {
-//         const { latitude, longitude } = position.coords;
-//         setQS('.long_lang', `[${longitude.toFixed(2)}, ${latitude.toFixed(2)}]`);
+const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August",
+                "September", "October", "November", "December"];
+                
+const thisDayInfo = () => {
+    const day = new Date();
+    setQS('.weekDay', days[day.getDay()]);
+    setQS('.monthDay', `${months[day.getMonth()]}, ${day.getDate()}`);
+    console.log(day)
+};
+
+const nextDayInfo = (QS, i) => {
+    const day = new Date(state.list[i].dt_txt.slice(0,10));
+    setQS(QS,`${days[day.getDay()]} - ${months[day.getMonth()]} - ${day.getDate()}`);
+};
+
+const callNextDayInfo = () => {
+    nextDayInfo('#mwd2',7);
+    nextDayInfo('#mwd3',15);
+    nextDayInfo('#mwd4',23);
+    nextDayInfo('#mwd5',31);
+    nextDayInfo('#mwd6',39)
+};
+
+const setStateOne = (data) => {
+    state.city = data.city.name;
+    state.temp = data.list[0].main.temp;
+    state.humidity = data.list[0].main.humidity;
+    state.country = data.city.country;
+    state.description = data.list[0].weather[0].description;
+    state.lat = data.city.coord.lat;
+    state.lon = data.city.coord.lon;
+    state.icon = data.list[0].weather[0].icon;
+    state.list = data.list;
+};
+
+const renderOneData = () => {
+    setQS('.city', `${state.city}, ${state.country}`);
+    setQS('.temp', `${Math.round(state.temp)} °C`);
+    setQS('.humidityGet', `${state.humidity}%`);
+    setQS('.cloud', `${state.description.charAt(0).toUpperCase() + state.description.slice(1)}`);
+    document.querySelector('.icon').src = `${API_IMG}${state.icon}.png`;
+    setQS('.long_lang', `[${state.lon.toFixed(2)}, ${state.lat.toFixed(2)}]`);
+};
+
+//---------------Get longitude and latitude------------------------------------
+
+(function getDate() {
+    window.navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        setQS('.long_lang', `[${longitude.toFixed(2)}, ${latitude.toFixed(2)}]`);
+        (async function () {
+            let response = await fetch( `${API_URL}forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
+            let data = await response.json();
+            console.log(data)
+            setStateOne(data);
+            renderOneData();
+            callNextDayInfo();
+        })();
+        thisDayInfo ();
+    });
+})();
+
+//avartvac che
+const setStateShow = (data, i) => {
+    state.temp = data.list[i].main.temp;
+    state.humidity = data.list[i].main.humidity;
+    state.country = data.city.country;
+    state.description = data.list[i].weather[i].description;
+    state.icon = data.list[i].weather[i].icon;
+    state.list = data.list;
+};
+
+const renderShowData = () => {
+    setQS('.time', `${state.city}`);
+    setQS('.temperature', `${Math.round(state.temp)} °C`);
+    setQS('.humGet', `${state.humidity}%`);
+    setQS('.desc', `${state.description.charAt(0).toUpperCase() + state.description.slice(1)}`);
+    document.querySelector('.iconList').src = `${API_IMG}${state.icon}.png`;
+};
+
+// const eventLis = (divName, i) => {
+//     document.querySelector(divName).addEventListener('mouseover', () => {
+//         document.querySelector('.show').style.display = "grid";
 //         (async function () {
-//             let response = await fetch( `${API_URL}forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
+//             const inputValue = document.getElementById('input').value;
+//             let response = await fetch( `${API_URL}forecast?q=${inputValue}&APPID=${API_KEY}&units=metric` );
 //             let data = await response.json();
-//             console.log(data)
-//             setStateOne(data);
-//             renderOneData();
-//             callNextDayInfo();
+//             setStateShow(data, i);
+//             renderShowData();
 //         })();
-//         thisDayInfo ();
 //     });
-// })();
-// //avartvac che
-// const setStateShow = (data, i) => {
-//     state.temp = data.list[i].main.temp;
-//     state.humidity = data.list[i].main.humidity;
-//     state.country = data.city.country;
-//     state.description = data.list[i].weather[i].description;
-//     state.icon = data.list[i].weather[i].icon;
-//     state.list = data.list;
+//     document.querySelector(divName).addEventListener('mouseout', () => {
+//         document.querySelector('.show').style.display = "none";
+//     });
 // };
-// const renderShowData = () => {
-//     setQS('.time', `${state.city}`);
-//     setQS('.temperature', `${Math.round(state.temp)} °C`);
-//     setQS('.humGet', `${state.humidity}%`);
-//     setQS('.desc', `${state.description.charAt(0).toUpperCase() + state.description.slice(1)}`);
-//     document.querySelector('.iconList').src = `${TEMP_URL}${state.icon}.png`;
-// };
-// // const eventLis = (divName, i) => {
-// //     document.querySelector(divName).addEventListener('mouseover', () => {
-// //         document.querySelector('.show').style.display = "grid";
-// //         (async function () {
-// //             const inputValue = document.getElementById('input').value;
-// //             let response = await fetch( `${API_URL}forecast?q=${inputValue}&APPID=${API_KEY}&units=metric` );
-// //             let data = await response.json();
-// //             setStateShow(data, i);
-// //             renderShowData();
-// //         })();
-// //     });
-// //     document.querySelector(divName).addEventListener('mouseout', () => {
-// //         document.querySelector('.show').style.display = "none";
-// //     });
-// // };
-// // eventLis('.two', 0);
-// // eventLis('.three');
-// // eventLis('.four');
-// // eventLis('.five');
-// // eventLis('.six');
-// //**********************************************************************
+
+// eventLis('.two', 0);
+// eventLis('.three');
+// eventLis('.four');
+// eventLis('.five');
+// eventLis('.six');
+//**********************************************************************
