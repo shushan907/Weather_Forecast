99c421206bcdbb9c0b8fdc0f119fbade98c4090c
Albert9991 <albert.poghosyan9991@gmail.com>
2020-06-08 13:31:57 +0300

    Modified Show Elements

--- javascript/script.js
+++ javascript/script.js
@@ -78,13 +78,39 @@ const gettingWeather = async () => {
 //----------------input keyup ENTER-------------------------------------
 
 const enter = (event) => {
-    if (event.key === 'Enter') gettingWeather()  
+    if (event.key === 'Enter') gettingWeather() 
+    time() 
 };
 
 //---------------------------Mouse------------------------------------------------
+
+const time = async() =>{
+    const inputValue = document.getElementById('input').value;
+    const response = await fetch( `${API_URL}forecast?q=${inputValue}&APPID=${API_KEY}&units=metric` );
+    const data = await response.json();
+    setState(data, 4);
+    renderData(7)
+    setState(data, 5);
+    renderData(8)
+    setState(data, 6);
+    renderData(9)
+    setState(data, 7);
+    renderData(10)
+    setState(data, 8);
+    renderData(11)
+    setState(data, 9);
+    renderData(12)
+    setState(data, 10);
+    renderData(13)
+    setState(data, 11);
+    renderData(14)
+}
+
+
 const mouse = (name) => {
     document.getElementById(name).addEventListener('mouseover', () => {
         document.getElementById('show').style.display = "grid";
+        time()
     });
     document.getElementById(name).addEventListener('mouseout', () => {
         document.getElementById('show').style.display = "none";
